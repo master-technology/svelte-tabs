b31e119dee7c0f681e463f4ed4cc24ba15c47728
Nathanael Anderson <nathan@master-technology.com>
2026-02-28 14:49:55 -0600

    Fix rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,5 +1,5 @@
 import svelte from "rollup-plugin-svelte";
-import resolve from "rollup-plugin-node-resolve";
+import resolve from "@rollup/plugin-node-resolve";
 import css from "rollup-plugin-import-css";
 import pkg from "./package.json" with { type: "json" };
 
